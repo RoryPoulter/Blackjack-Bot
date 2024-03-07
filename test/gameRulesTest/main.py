61947b8c0198bcd5b60becfebe8ff46719de1788
Rory Poulter <rorypoulter2@gmail.com>
2024-03-07 17:17:35 +0000

    Update main.py
    New methods in Hand class:
    - burn
    - split
    - __eq__
    - __len__

--- test/gameRulesTest/main.py
+++ test/gameRulesTest/main.py
@@ -1,17 +1,18 @@
-# from random import randint  # Not used during testing
+from random import randint
 from numpy import array
 
 
 class Hand:
-    def __init__(self, card_1, card_2):
+    def __init__(self, *cards):
         """
-        :param card_1: The first card drawn
-        :type card_1: Card
-        :param card_2: The second card drawn
-        :type card_2: Card
+        :param card_1: The cards drawn
+        :type cards: Card
         """
-        self.total: array = card_1.value + card_2.value
-        self.cards: list[Card] = [card_1, card_2]
+        self.total: array = array([0, 0])
+        for card in cards:
+            self.total += card.value
+            self.checkTotal()
+        self.cards: list[Card] = [*cards]
 
     def __repr__(self):
         return ", ".join(list(map(str, self.cards)))
@@ -31,6 +32,27 @@ class Hand:
             return self.total[1] > other
         else:
             return self.total[1] > other.total[1]
+
+    def __eq__(self, other):
+        """
+        Compares the totals of the hands
+        :param other: The other hand object or a number
+        :type other: Hand | int
+        :return: whether the first hand has an equal total
+        :rtype: bool
+        """
+        if type(other) is int:
+            return self.total[1] == other
+        else:
+            return self.total[1] == other.total[1]
+
+    def __len__(self):
+        """
+        Finds the number cards in the hand
+        :return: The number of cards in the hand
+        :rtype: int
+        """
+        return len(self.cards)
 
     def drawCard(self, new_card):
         """
@@ -55,13 +77,31 @@ class Hand:
             self.total[1] = self.total[0]  # Sets the max total == min total
         return True
 
+    def split(self):
+        if (self.cards[0].face == self.cards[1].face) and (len(self) == 2):
+            second_hand: Hand = Hand(self.cards[1])
+            self.total = self.cards[0].value
+            self.cards = self.cards[:1]
+            return second_hand
+
+    def burn(self):
+        if self.total[1] == 14:
+            no_of_cards: int = len(self)
+            new_cards: dict[int, Card] = {}
+            for i in range(no_of_cards):
+                new_cards[i] = Card(randint(0, 51))
+            self.__init__(*new_cards.values())
+
 
 class Card:
     def __init__(self, card_number):
         """
         :param card_number: Random number to determine suit and face
         :type card_number: int
+        :raise ValueError: If card_number is out of range
         """
+        if card_number > 51 or card_number < 0:
+            raise ValueError("card_number must be between 0 and 51 inclusive")
         self.suit: str = SUITS[card_number // 13]
         self.face: str = FACES[card_number % 13][0]
         self.value: array = FACES[card_number % 13][1]
@@ -71,8 +111,8 @@ class Card:
 
 
 if __name__ == "__main__":
-    SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
-    FACES = [
+    SUITS: list[str] = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    FACES: list[list[str, array]] = [
         ["Ace", array([1, 11])],
         ["2", array([2, 2])],
         ["3", array([3, 3])],
@@ -88,14 +128,10 @@ if __name__ == "__main__":
         ["King", array([10, 10])],
     ]
 
-    card_1 = Card(0)
-    print(card_1)
-    card_2 = Card(11)
-    print(card_2)
-    hand = Hand(card_1, card_2)
-    print(hand)
-    print(hand.total)
-    card_3 = Card(2)
-    hand.drawCard(card_3)
-    print(hand)
-    print(hand.total)
+    card_1: Card = Card(1)  # 2 of Spades
+    card_2: Card = Card(12)  # King of Spades
+    card_3: Card = Card(1)  # 2 of Spades
+    hand: Hand = Hand(card_1, card_2, card_3)
+    print(hand, hand.total)
+    hand.burn()
+    print(hand, hand.total)
