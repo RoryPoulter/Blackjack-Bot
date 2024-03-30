1d56a3dd20402181787b15f268cedb90771845ca
Rory Poulter <rorypoulter2@gmail.com>
2024-03-30 13:13:23 +0000

    Update main.py
    Improved documentation with Literals for gameOver method

--- test/gameRulesTest/main.py
+++ test/gameRulesTest/main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from hand import Hand
+from typing import Literal
 
 
 class Game:
@@ -16,13 +17,19 @@ class Game:
 
         if self.dealer_hand.blackjack:
             print(self.dealer_hand)
-            self.gameOver("dealer", "blackjack")
+            self.gameOver("Dealer", "blackjack")
         elif self.player_hand.blackjack:
-            self.gameOver("player", "blackjack")
+            self.gameOver("Player", "blackjack")
         else:
             self.playerTurn()
 
-    def gameOver(self, winner, win_condition):
+    def gameOver(self,
+                 winner: Literal["Player", "Dealer"],
+                 win_condition: Literal["blackjack", "5 card", "21", "greater", "bust"]):
+        """Displays a message at the end of the game detailing who won and how
+        :param winner: Who won the game, either `'player'` or `'dealer'`
+        :param win_condition: How the winner won the game
+        """
         messages = {
             "blackjack": f"{winner} won with blackjack!",
             "5 card": f"{winner} won with 5 cord trick!",
@@ -36,15 +43,17 @@ class Game:
         """Determines the winner of the game
         """
         if len(self.dealer_hand) == 5:
-            self.gameOver("dealer", "5 card")
+            self.gameOver("Dealer", "5 card")
         elif len(self.player_hand) == 5:
-            self.gameOver("player", "5 card")
+            self.gameOver("Player", "5 card")
         elif self.player_hand > self.dealer_hand:
-            self.gameOver("player", "greater")
+            self.gameOver("Player", "greater")
         else:
-            self.gameOver("dealer", "greater")
+            self.gameOver("Dealer", "greater")
 
     def playerTurn(self):
+        """Allows the player to draw a card or end their turn
+        """
         draw = input("Draw card? [y/n]: ").upper()
         if draw == "Y":
             sleep(0.5)
@@ -52,7 +61,7 @@ class Game:
             print(self.player_hand)
             if not self.player_hand.in_play:
                 print(self.dealer_hand)
-                self.gameOver("dealer", "bust")
+                self.gameOver("Dealer", "bust")
             elif (len(self.player_hand) == 5) or (self.player_hand == 21):
                 self.dealerTurn()
             else:
@@ -61,22 +70,20 @@ class Game:
             self.dealerTurn()
 
     def dealerTurn(self):
+        """Dealer draws cards until their total is equal to or greater than 17
+        """
         print(self.dealer_hand)
         while self.dealer_hand < 17:
             sleep(0.5)
             self.dealer_hand.drawCard()
             print(self.dealer_hand)
         if self.dealer_hand == 21:
-            self.gameOver("dealer", "21")
+            self.gameOver("Dealer", "21")
         elif self.dealer_hand > 21:
-            self.gameOver("player", "bust")
+            self.gameOver("Player", "bust")
         else:
             self.decideWinner()
 
 
-def main():
-    Game()
-
-
 if __name__ == "__main__":
-    main()
+    Game()
