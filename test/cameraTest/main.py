889b6de9f8c94e77b8811991800f9ecdbbb0e321
RoryPoulterPi <rorypoulter2@gmail.com>
2024-05-15 20:28:16 +0100

    Add code to turn camera on and off

--- test/cameraTest/main.py
+++ test/cameraTest/main.py
@@ -1,10 +1,15 @@
 """Test for using the RaspberryPi camera to read the faces of the cards and create objects from the data"""
-import card
+# import card
+from picamera import PiCamera
+from time import sleep
 
 
 def main():
-    ...
-# this is a test 
+    camera = PiCamera()  # Creates PiCamera object
+    camera.start_preview()  # Turns on the camera
+    sleep(5)  # Waits 5 seconds
+    camera.stop_preview()  # Turns off the camera
+    
 
 if __name__ == "__main__":
-    ...
+    main()
