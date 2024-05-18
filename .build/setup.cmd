f0a120907700acb91dd7f1543275c3e8f42a0f68
RoryPoulterPi <rorypoulter2@gmail.com>
2024-05-18 22:08:28 +0100

    Update setup.cmd

--- .build/setup.cmd
+++ .build/setup.cmd
@@ -1,6 +1,6 @@
 REM Installs the libraries from requirements.txt
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 REM Changes directory to src\
 cd ..\src\
 REM Runs the main script
-python main.py
+python3 main.py
