a07b2b40b412492a874e66a3e9013459ff84de01
Rory Poulter <rorypoulter2@gmail.com>
2024-05-22 19:20:39 +0100

    Create test directory for setup.sh

--- test/setupTest/setup.sh
+++ test/setupTest/setup.sh
@@ -1,6 +1,7 @@
 #TODO script to set up Python 3.9 as default version
 # Sets current directory to ~/.build/
 cd "$(dirname "$0")"
+cd ../../.build/ # Not in final version
 sudo pip3 install --upgrade pip3 setuptools wheel
 # Installs the dependencies
 sudo pip3 install -r requirements.txt
