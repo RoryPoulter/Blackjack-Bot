63761bd3cd9c48a2bcec6504d2621ab2d0740401
Rory Poulter <rorypoulter2@gmail.com>
2024-05-23 16:37:02 +0100

    Update setup.sh to upgrade Python to 3.12.3

--- test/setupTest/setup.sh
+++ test/setupTest/setup.sh
@@ -5,17 +5,32 @@ ver="$(python -V)"
 if [ "$ver" = "Python 3.12.3" ]; then
     echo "Correct version"
 else
-    echo "Python needs updating to 3.12"
+    # Download source code
+    wget https://www.python.org/ftp/python/3.12.3/Python-3.12.3.tgz
+    # Install build tools
+    sudo apt update
+    sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev  libsqlite3-dev
+    # Configure, make and install
+    tar -xzvf Python-3.12.3.tgz 
+    cd Python-3.12.3/
+    ./configure
+    sudo make altinstall
+    # Create a softlink
+    sudo rm /usr/bin/python
+    sudo ln -s /usr/local/bin/python3.12 /usr/bin/python
+    # Deletes the installation directory
+    cd /tmp/
+    rm -rf rm -rf Python-3.12.3.tgz Python-3.12.3/
 fi
 
 
 # Sets current directory to ~/.build/
 cd "$(dirname "$0")"
 cd ../../.build/ # Not in final version
-sudo pip3 install --upgrade pip3 setuptools wheel
+sudo pip install --upgrade pip3 setuptools wheel
 # Installs the dependencies
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 # Changes the current directory to ~/src/
 cd ../src/
 # Runs the main code
-sudo python3 main.py
+sudo python main.py
