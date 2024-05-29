57a089dfe77c4b4d949bbd1e925ea52e61338c46
RoryPoulterPi <rorypoulter2@gmail.com>
2024-05-29 16:29:45 +0100

    Update error message in cameraTest/main.py when camera is not detected

--- test/cameraTest/main.py
+++ test/cameraTest/main.py
@@ -8,7 +8,15 @@ def main():
     try:
         camera = PiCamera()  # Creates PiCamera object
     except Exception:
-        print("Error: Camera not connected")
+        print("""Error: Camera not detected.
+If a camera is not installed: 
+    1. Turn off Raspberry Pi
+    2. Install camera
+    3. Turn back on.
+If a camera is installed:
+    1. Type 'sudo raspi-config' in the terminal
+    2. Select 'Interface Options'
+    3. Enable Legacy Camera support""")
         return
     camera.start_preview()  # Turns on the camera
     sleep(5)  # Waits 5 seconds
