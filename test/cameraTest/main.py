633b68a0fad8117e2fc73e40a9829e5570028ce8
Rory Poulter <rorypoulter2@gmail.com>
2024-06-04 23:45:20 +0100

    Remove code from test/cameraTest/main.py

--- test/cameraTest/main.py
+++ test/cameraTest/main.py
@@ -1,24 +1,24 @@
 """Test for using the RaspberryPi camera to read the faces of the cards and create objects from the 
 data. Currently does not read text on cards."""
 # import card
-from time import sleep
+# from time import sleep
 
-from picamera import PiCamera
+# from picamera import PiCamera
 
-from error import cameraError
+# from error import cameraError
 
-def main():
-    """The main body of code
-    """
-    try:
-        camera = PiCamera()  # Creates PiCamera object
-    except Exception:
-        cameraError()
-        return
-    camera.start_preview()  # Turns on the camera
-    sleep(5)  # Waits 5 seconds
-    camera.stop_preview()  # Turns off the camera
+# def main():
+#     """The main body of code
+#     """
+#     try:
+#         camera = PiCamera()  # Creates PiCamera object
+#     except Exception:
+#         cameraError()
+#         return
+#     camera.start_preview()  # Turns on the camera
+#     sleep(5)  # Waits 5 seconds
+#     camera.stop_preview()  # Turns off the camera
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
